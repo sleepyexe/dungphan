203ca57d46b1b3f4f8420c8598173f832294f62c
sleepyexe <67649181+sleepyexe@users.noreply.github.com>
2021-09-16 23:35:31 +0700

    Update lab6cfree.bat

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -5,17 +5,17 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v Dis
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f
-certutil -urlcache -split -f https://raw.githubusercontent.com/dungphanx2/dungphan/master/curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/dungphanx2/dungphan/master/7z.dll
-certutil -urlcache -split -f https://raw.githubusercontent.com/dungphanx2/dungphan/master/7z.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/dungphanx2/dungphan/master/nircmd.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/sleepyexe/dungphan/master/curl.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/sleepyexe/dungphan/master/7z.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/sleepyexe/dungphan/master/7z.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/sleepyexe/dungphan/master/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 
 
 cd C:\Users\Administrator\Desktop
-curl -LJOk https://raw.githubusercontent.com/dungphanx2/dungphan/master/ChromeSetup.exe
-curl -LJOk https://raw.githubusercontent.com/dungphanx2/dungphan/master/anti-close.bat
+curl -LJOk https://raw.githubusercontent.com/sleepyexe/dungphan/master/ChromeSetup.exe
+curl -LJOk https://raw.githubusercontent.com/sleepyexe/dungphan/master/anti-close.bat
 start ChromeSetup.exe
 cls
 @echo off
